76f05e20b659543e1f93ec1dd4f1fbcd9fca92a6
bayan2002 <bayanabdelbaryq@gmail.com>
2022-09-06 10:10:33 +0300

    create authentication middleware releates #17

---
+++ src/middleware/authenticate.js
@@ -0,0 +1,18 @@
+require('env2').apply('.env');
+const { CustomError, verifyToken } = require('../utils');
+
+const authenticateToken = (req, res, next) => {
+  const { token } = req.cookie;
+  if (!token) {
+    throw CustomError('unauthorized', 400);
+  } else {
+    verifyToken(token, process.env.SECRET_KEY)
+      .then((decoded) => {
+        req.body.id = decoded.id;
+        next();
+      })
+      .catch((err) => next(err));
+  }
+};
+
+module.exports = authenticateToken;
---
+++ src/middleware/index.js
@@ -0,0 +1,3 @@
+const authenticateToken = require('./authenticate');
+
+module.exports = { authenticateToken };
