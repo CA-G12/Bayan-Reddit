490a2ef61a9c3448e3834be4ceb302317bc48b18
bayan2002 <bayanabdelbaryq@gmail.com>
2022-09-09 09:52:04 +0300

    Handle profile page releates #23

---
+++ src/database/queries/profile.js
@@ -0,0 +1,10 @@
+const connection = require('../config/connection');
+
+const profilePosts = (id) => connection.query({
+    text: 'select p.id, p.title, p.content, p.img, p.date, u.id ,u.user_name, (select count(c.id) from comments c where c.post_id =p.id) as commentCount,(SELECT SUM(v.vote_value) FROM votes v WHERE v.post_id = p.id) AS votes_counts from posts as p JOIN users as u on u.id = p.user_id where u.id = $1;',
+    values: [id],
+}
+  
+);
+
+module.exports = profilePosts;
